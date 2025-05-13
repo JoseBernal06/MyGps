17ccbccd8eeb1c65e4883e178bf73bf0f7ffb44b
Jose Mateo Bernal <150805766+JoseBernal06@users.noreply.github.com>
2025-05-13 14:57:48 -0500

    primera version

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  standalone: false,
 })
 export class HomePage {
+  latitude: number | null = null;
+  longitude: number | null = null;
 
   constructor() {}
 
+  async getCurrentLocation() {
+    try {
+      const coordinates = await Geolocation.getCurrentPosition();
+      this.latitude = coordinates.coords.latitude;
+      this.longitude = coordinates.coords.longitude;
+    } catch (error) {
+      console.error('Error obteniendo ubicacion', error);
+    }
+  }
 }
+
